13765c81de23e1365e74e4a090669be69af99b1c
ZloyenotVN <68386904+ZloyenotVN@users.noreply.github.com>
2021-03-26 16:09:59 +0300

    Update solver3.m
    Поменял используемую функцию расчета

--- solver3.m
+++ solver3.m
@@ -1,7 +1,11 @@
-function [x1, x2] = solver3(XpT, PD, h_LA)
+function [X1,X2] = solver3(XpT, PD, h_LA)
 
-    [X] = X_function2D_v0(PD,XpT,3);
-    [ x1(1),x1(2)] = Navigate3or4( XpT, PD, h_LA, X(2,1),X(3,1) );
-    [ x2(1),x2(2)] = Navigate3or4( XpT, PD, h_LA, X(2,2),X(3,2) );
+    [X0] = X_function2D_v0(PD,XpT,3);
+%     [ x1(1),x1(2)] = Navigate3or4( XpT, PD, h_LA, X0(2,1),X0(3,1) );
+%     [ x2(1),x2(2)] = Navigate3or4( XpT, PD, h_LA, X0(2,2),X0(3,2) );
+
+      [X1, dop1] = coord_solver2D(PD, XpT, [X0(2:3,1)' PD(1,1)]', h_LA);
+      [X2, dop2] = coord_solver2D(PD, XpT, [X0(2:3,2)' PD(1,1)]', h_LA);
+      
 end
 
