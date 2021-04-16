2dfdfd38d45a877a44d17519102c595db6becde3
ZloyenotVN <68386904+ZloyenotVN@users.noreply.github.com>
2021-04-16 16:24:22 +0300

    Update poi_test.m
    добавил описание строчек

--- poi_test.m
+++ poi_test.m
@@ -1,11 +1,11 @@
 function [database] = poi_test(poits)
     database = [];
     for i = 1:length(poits)
-        if poits(i).freq == 1090000 && poits(i).count > 2
+        if poits(i).freq == 1090000 && poits(i).count > 2 %обязательно больше двух отождествлений на постах
             ID = poits(i).d4c1;
             if isempty(database)
                 database(1).ID = ID;
-                database(1).k = 1;
+                database(1).k = 1; %число отметок в завязке
                 database(1).data(:,database(1).k) = poits(i).imps(:,2);
             else
                 match_flag = 0;
@@ -17,11 +17,11 @@ function [database] = poi_test(poits)
                 end
                 
                 if match_flag
-                    database(j).k = database(j).k + 1;
+                    database(j).k = database(j).k + 1; %число отметок в завязке
                     database(j).data(:,database(j).k) = poits(i).imps(:,2);
                 else
                     database(j + 1).ID = ID;
-                    database(j + 1).k = 1;
+                    database(j + 1).k = 1; %число отметок в завязке
                     database(j + 1).data(:,database(j+1).k) = poits(i).imps(:,2);
                 end
             end
