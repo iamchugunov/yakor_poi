10f9fb96cc2dea5c1cb1c8836ccd80e80e550a41
Nikitos <nekitpetuhov@yandex.ru>
2021-05-21 13:50:05 +0300

    Анализатор корреляции v1

--- make_corr_massives.m
+++ make_corr_massives.m
@@ -4,28 +4,32 @@ function [a1, a2, a3, a4] = make_corr_massives(frame, config)
     a1 = zeros(1,length(t));
     for i = 1:length(frame.Post1)
         if frame.Post1(i).freq > 1090000
-            a1(frame.Post1(i).uT) = frame.Post1(i).uT;
+            %a1(frame.Post1(i).uT) = frame.Post1(i).uT;
+            a1(frame.Post1(i).uT) = 1;
         end
     end
     
     a2 = zeros(1,length(t));
     for i = 1:length(frame.Post2)
         if frame.Post2(i).freq > 1090000
-            a2(frame.Post2(i).uT) = frame.Post2(i).uT;
+            %a2(frame.Post2(i).uT) = frame.Post2(i).uT;
+            a2(frame.Post2(i).uT) = 1;
         end
     end
     
     a3 = zeros(1,length(t));
     for i = 1:length(frame.Post3)
         if frame.Post3(i).freq > 1090000
-            a3(frame.Post3(i).uT) = frame.Post3(i).uT;
+            %a3(frame.Post3(i).uT) = frame.Post3(i).uT;
+            a3(frame.Post3(i).uT) = 1;
         end
     end
     
     a4 = zeros(1,length(t));
     for i = 1:length(frame.Post4)
         if frame.Post4(i).freq > 1090000
-            a4(frame.Post4(i).uT) = frame.Post4(i).uT;
+            %a4(frame.Post4(i).uT) = frame.Post4(i).uT;
+            a4(frame.Post4(i).uT) = 1;
         end
     end
 end
